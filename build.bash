b8be12c0a66797b7a71f1edb468b948da2203d50
Ringo Hoffmann <riho@live.de>
2018-04-26 14:42:39 +0200

    init

---
+++ build.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if ! type g++ >/dev/null 2>&1
+then
+    echo "g++ is not installed or can not be accessed as command."
+    exit -1
+fi
+
+echo "Building cgit..."
+g++ gitpluscloc.cpp -o cgit
+
+echo "Installing cgit to '/usr/bin/cgit'..."
+mv cgit /usr/bin/cgit
+
+echo "Build and installation finished."
+echo "You can now access gitpluscloc with 'cgit'."
+echo "Every git command you can also use with 'cgit'."
+echo "When you commit with 'cgit', it will automatically create a cloc file and pushing it into current branch."
---
+++ gitpluscloc.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <stdio.h>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+/*########### PREFERENCES ###########*/
+#define CLOC_LOG_FILE "./CLOC_LOG.md"
+#define CLOC_ARGUMENTS "--md"
+/*###################################*/
+
+
+#define VERSION "1.0"
+
+
+
+void pause() {
+    cin.clear();
+    cin.get();
+}
+
+
+string exec(const char* cmd) {
+    cout << "$ " << cmd << endl;
+    char buffer[1024];
+    string result = "";
+    FILE* pipe = _popen(cmd, "r");
+    if (!pipe)
+        throw runtime_error("_popen() failed");
+    try {
+        while (!feof(pipe)) {
+            if (fgets(buffer, 1024, pipe) != NULL)
+                result += buffer;
+        }
+    }
+    catch (...) {
+        _pclose(pipe);
+        throw;
+    }
+    _pclose(pipe);
+    return result;
+}
+
+
+bool cmd_option_exists(char** begin, char** end, const std::string& option) {
+    return std::find(begin, end, option) != end;
+}
+
+
+void create_cloc() {
+    cout << "Creating '" << CLOC_LOG_FILE << "' file...\n";
+    string cloccmd = "cloc ./ ";
+    cloccmd += CLOC_ARGUMENTS;
+    string execout = exec(cloccmd.c_str());
+    ofstream fw(CLOC_LOG_FILE);
+    fw << execout;
+    fw.close();
+}
+
+
+int main(int argc, char** argv) {
+
+    if (cmd_option_exists(argv, argv + argc, "--help")) {
+        cout << endl
+             << "gitpluscloc (cgit) v." << VERSION << endl
+             << "(c) 2018 Ringo Hoffmann (zekro Development)\n\n"
+             << "This tool does just pass all arguments given through to git\n" 
+             << "and extending commit with adding cloc document.\n";
+        return 0;
+    }
+
+    string git_cmd = "git ";
+    for (int i = 1; i < argc; i++) {
+        string tmp = argv[i];
+        if (tmp.find(" ", 0) < 4294967295) {
+            git_cmd += "\"";
+            git_cmd += argv[i];
+            git_cmd += "\"";
+        }
+        else
+            git_cmd += argv[i];
+        git_cmd += " ";
+    }
+    
+    if (cmd_option_exists(argv, argv + argc, "commit")) {
+        create_cloc();
+        string _cmd = "git add ";
+        _cmd += CLOC_LOG_FILE;
+        cout << exec(_cmd.c_str());
+    }
+    cout << exec(git_cmd.c_str());
+}
